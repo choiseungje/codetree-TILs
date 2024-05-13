f6c0814bf441b3063b62bce1372a8105a9018ca7
choiseungje <161275179+choiseungje@users.noreply.github.com>
2024-05-13 11:12:38 +0900

    Title: 진법 변환, Time: 235ms, Memory: 24MB, Status: Passed - CodeTree

--- 240513/진법 변환/base-conversion.py
+++ 240513/진법 변환/base-conversion.py
@@ -1,13 +1,13 @@
 lst = [chr(i+65) for i in range(26)]
 lst = ['0','1','2','3','4','5','6','7','8','9'] + lst
 a,b = input().split()
-k = 0
-for idx,i in enumerate(lst):
-    if i == b:
-        k = idx
+b = int(b)
+lst = lst[:b+1]
+k = len(a) - 1
 answer = 0
-p = 0
-for i in range(len(a)):
-    answer += int(a[-(i+1)])*k**p
-    p += 1
+for i in a:
+    for u in lst:
+        if i == u:
+            answer += lst.index(u) * b ** k
+            k -= 1
 print(answer)
