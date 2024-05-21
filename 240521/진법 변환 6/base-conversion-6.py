91d95b6601fe11c6820741a22a0717119b84a015
choiseungje <161275179+choiseungje@users.noreply.github.com>
2024-05-21 16:20:54 +0900

    Title: 진법 변환 6, Time: 154ms, Memory: 24MB, Status: Passed - CodeTree

--- 240521/진법 변환 6/base-conversion-6.py
+++ 240521/진법 변환 6/base-conversion-6.py
@@ -2,7 +2,7 @@ k = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i'
 a,b,c = input().split()
 sum = 0
 for i in range(len(b)):
-    sum += int(b[i]) * int(a)**(len(b)-i-1)
+    sum += int(k.index(b[i])) * int(a)**(len(b)-i-1)
 answer = ''
 while sum > 0:
     answer = k[sum%int(c)] + answer
