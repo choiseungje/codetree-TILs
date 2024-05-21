3dd7c05a9ec69d26c5b9679f300debf3703006b6
choiseungje <161275179+choiseungje@users.noreply.github.com>
2024-05-21 16:39:25 +0900

    Title: 등차 수열, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240521/등차 수열/an-arithmetic-sequence.py
+++ 240521/등차 수열/an-arithmetic-sequence.py
@@ -3,11 +3,12 @@ k = list(map(int,input().split()))
 try:
     p = k.index(a)
     k = k[p:]
-answer = 0
-while True:
-    if a in k:
-        answer += 1
-        a += b
-    else:
-        break
-print(answer)
+finally:
+    answer = 0
+    while True:
+        if a in k:
+            answer += 1
+            a += b
+        else:
+            break
+    print(answer)
