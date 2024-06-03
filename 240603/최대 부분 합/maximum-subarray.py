6f8d8578754cf58c24183521397c4d5831a2cfa8
choiseungje <161275179+choiseungje@users.noreply.github.com>
2024-06-03 17:55:17 +0900

    Title: 최대 부분 합, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240603/최대 부분 합/maximum-subarray.py
+++ 240603/최대 부분 합/maximum-subarray.py
@@ -1,9 +1,13 @@
 a = input()
 b = list(map(int,input().split()))
 count = 0
+lst = []
 if max(b) < 0:
     count = max(b)
 for i in b:
     if i >= 0:
         count += i
+    else:
+        lst.append(count)
+        count = 0
 print(count)
